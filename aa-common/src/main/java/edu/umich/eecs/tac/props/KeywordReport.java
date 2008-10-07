656da033df5b089e69e92629aaf4baf1cefff903
bcassell <bcassell@a0f66c25-10ba-420b-b248-e489c366c075>
2008-10-07 22:22:04 +0000

    worked on KeywordReport, awaiting discussion for how to finalize

--- aa-common/src/main/java/edu/umich/eecs/tac/props/KeywordReport.java
+++ aa-common/src/main/java/edu/umich/eecs/tac/props/KeywordReport.java
@@ -1,6 +1,7 @@
 package edu.umich.eecs.tac.props;
 import java.io.Serializable;
 import java.text.ParseException;
+import java.util.LinkedList;
 
 import se.sics.isl.transport.TransportReader;
 import se.sics.isl.transport.TransportWriter;
@@ -14,9 +15,10 @@ public class KeywordReport implements Serializable, Transportable {
 	 */
 	private static final long serialVersionUID = -4366560152538990286L;
 
+	LinkedList<QueryReport> yesterdaysReport = new LinkedList<QueryReport>();
+	
 	public String getTransportName() {
-		// TODO Auto-generated method stub
-		return null;
+		return "keywordreport";
 	}
 
 	public void read(TransportReader reader) throws ParseException {
@@ -29,4 +31,61 @@ public class KeywordReport implements Serializable, Transportable {
 
 	}
 
+}
+
+class QueryReport {
+
+	private String queryString = new String();
+	private int impressions;
+	private int clicks;
+	private int conversions;
+	private int position;
+	private float cost;
+	private float revenue;
+	
+	public QueryReport(String s){
+		queryString = s;
+	}
+	
+	public String getQueryString() {
+		return queryString;
+	}
+	
+	public int getImpressions() {
+		return impressions;
+	}
+	public void setImpressions(int impressions) {
+		this.impressions = impressions;
+	}
+	public int getClicks() {
+		return clicks;
+	}
+	public void setClicks(int clicks) {
+		this.clicks = clicks;
+	}
+	public int getConversions() {
+		return conversions;
+	}
+	public void setConversions(int conversions) {
+		this.conversions = conversions;
+	}
+	public int getPosition() {
+		return position;
+	}
+	public void setPosition(int position) {
+		this.position = position;
+	}
+	public float getCost() {
+		return cost;
+	}
+	public void setCost(float cost) {
+		this.cost = cost;
+	}
+	public float getRevenue() {
+		return revenue;
+	}
+	public void setRevenue(float revenue) {
+		this.revenue = revenue;
+	}
+	
 }
