a52a3ed3b6815969b4242dff8d9ca013d49d38c3
prjordan <prjordan@a0f66c25-10ba-420b-b248-e489c366c075>
2009-01-09 18:29:14 +0000

    fixed bug in pricing

--- aa-common/src/main/java/edu/umich/eecs/tac/props/AAInfo.java
+++ aa-common/src/main/java/edu/umich/eecs/tac/props/AAInfo.java
@@ -53,7 +53,7 @@ public class AAInfo implements ContextFactory {
         context.addTransportable(new BidBundle());
         context.addTransportable(new BidBundle.BidEntry());
         context.addTransportable(new Ranking());
-        context.addTransportable(new Pricing());
+        context.addTransportable(new Pricing());        
         context.addTransportable(new UserClickModel());
         context.addTransportable(new Auction());
         context.addTransportable(new AdvertiserInfo());
--- aa-common/src/main/java/edu/umich/eecs/tac/props/Pricing.java
+++ aa-common/src/main/java/edu/umich/eecs/tac/props/Pricing.java
@@ -2,6 +2,7 @@ package edu.umich.eecs.tac.props;
 
 import se.sics.isl.transport.TransportReader;
 import se.sics.isl.transport.TransportWriter;
+import se.sics.isl.transport.Transportable;
 
 import java.util.List;
 import java.util.Map;
@@ -13,26 +14,28 @@ import java.text.ParseException;
  * @author Patrick Jordan
  */
 public class Pricing extends AbstractTransportable {
-    private Map<AdLink,Double> prices;
+    private static final String PRICE_ENTRY_TRANSPORT_NAME = "PriceEntry";
+
+    private Map<AdLink, Double> prices;
 
 
     public Pricing() {
-        prices = new HashMap<AdLink,Double>();
+        prices = new HashMap<AdLink, Double>();
     }
 
     public void setPrice(AdLink ad, double price) throws NullPointerException {
         lockCheck();
 
-        if(ad==null)
+        if (ad == null)
             throw new NullPointerException("ad cannot be null");
-        
-        prices.put(ad,price);
+
+        prices.put(ad, price);
     }
 
     public double getPrice(AdLink ad) {
         Double price = prices.get(ad);
 
-        if(price==null)
+        if (price == null)
             return Double.NaN;
         else
             return price;
@@ -44,27 +47,51 @@ public class Pricing extends AbstractTransportable {
 
     protected void readWithLock(TransportReader reader) throws ParseException {
         prices.clear();
-        while(reader.nextNode("PriceEntry", false)) {
-            double price = reader.getAttributeAsDouble("price",Double.NaN);
-            AdLink ad = null;
-            if(reader.nextNode(AdLink.class.getSimpleName(),false)) {
-                ad = (AdLink)reader.readTransportable();
-            }
-
-            if(ad!=null) {
-                setPrice(ad,price);
-            }
+        while (reader.nextNode(PRICE_ENTRY_TRANSPORT_NAME, false)) {
+            readPriceEntry(reader);
         }
     }
 
     protected void writeWithLock(TransportWriter writer) {
-        for (Map.Entry<AdLink,Double> entry : prices.entrySet()) {
-            writer. node("PriceEntry");
-            if(entry.getValue()!=null)
-                writer.attr("price",entry.getValue());
-            if(entry.getKey()!=null)
-                writer.write(entry.getKey());
-            writer.endNode("PriceEntry");
+        for (Map.Entry<AdLink, Double> entry : prices.entrySet()) {
+            writePriceEntry(writer,entry.getValue(),entry.getKey());
         }
+    }
+
+    protected void writePriceEntry(TransportWriter writer, Double price, AdLink adLink) {
+        String nodeName = PRICE_ENTRY_TRANSPORT_NAME;
+        writer.node(nodeName);
+
+        int nodeLevel = writer.getNodeLevel();
+
+        if(price!=null) {
+            writer.attr("price", price);
+        }
+        
+        if (adLink != null)
+            writer.write(adLink);
+        if (nodeLevel != writer.getNodeLevel()) {
+            throw new IllegalStateException("wrong node level " + writer.getNodeLevel() + " (expected "
+                    + nodeLevel + ") for transportable " + PRICE_ENTRY_TRANSPORT_NAME);
+        }
+
+        writer.endNode(nodeName);
+    }
+
+    protected void readPriceEntry(TransportReader reader) throws ParseException {
+        reader.enterNode();
+
+        double price = reader.getAttributeAsDouble("price", Double.NaN);
+
+        AdLink adLink = null;
+        if (reader.nextNode(AdLink.class.getSimpleName(), false)) {
+            adLink = (AdLink) reader.readTransportable();
+        }
+
+        if (adLink != null) {
+            setPrice(adLink, price);
+        }
+
+        reader.exitNode();
     }
 }
