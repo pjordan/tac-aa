5b3399d2bfb9fcaf39af90df312f55f628e68152
bcassell <bcassell@a0f66c25-10ba-420b-b248-e489c366c075>
2009-01-20 19:31:17 +0000

    added DefaultUsersBehaviorTest

---
+++ aa-server/src/test/java/edu/umich/eecs/tac/user/DefaultUsersBehaviorTest.java
@@ -0,0 +1,78 @@
+package edu.umich.eecs.tac.user;
+
+/**
+ * @author Ben Cassell
+ */
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class DefaultUsersBehaviorTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testDefaultUsersBehavior() {
+
+	}
+
+	@Test
+	public void testNextTimeUnit() {
+
+	}
+
+	@Test
+	public void testSetup() {
+
+	}
+
+	@Test
+	public void testCreateBuilder() {
+
+	}
+
+	@Test
+	public void testStopped() {
+
+	}
+
+	@Test
+	public void testShutdown() {
+
+	}
+
+	@Test
+	public void testGetRanking() {
+
+	}
+
+	@Test
+	public void testMessageReceived() {
+
+	}
+
+	@Test
+	public void testAddUserEventListener() {
+
+	}
+
+	@Test
+	public void testContainsUserEventListener() {
+
+	}
+
+	@Test
+	public void testRemoveUserEventListener() {
+
+	}
+
+	@Test
+	public void testBroadcastUserDistribution() {
+
+	}
+
+}
