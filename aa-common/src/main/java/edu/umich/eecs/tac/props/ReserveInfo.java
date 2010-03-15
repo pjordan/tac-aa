362b278fe4a70809fad8b85c83e17b3a7c7a552e
akaul <akaul@a0f66c25-10ba-420b-b248-e489c366c075>
2010-03-15 17:53:04 +0000

    Changes to reserve prices

--- aa-common/src/main/java/edu/umich/eecs/tac/props/ReserveInfo.java
+++ aa-common/src/main/java/edu/umich/eecs/tac/props/ReserveInfo.java
@@ -28,6 +28,7 @@ import se.sics.isl.transport.TransportReader;
 import se.sics.isl.transport.TransportWriter;
 
 import java.text.ParseException;
+import java.util.Arrays;
 
 /**
  * This class contains the auction reserve information released to the user and publisher agents at the begining of the
@@ -40,19 +41,24 @@ public class ReserveInfo extends AbstractTransportable {
     /**
      * The promoted reserve score (see <a href="http://aa.tradingagents.org/documentation">TAC Documentation</a>).
      */
-    private double promotedReserve;
+    private double[] promotedReserve;
     /**
      * The regular reserve score (see <a href="http://aa.tradingagents.org/documentation">TAC Documentation</a>).
      */
-    private double regularReserve;
+    private double[] regularReserve;
 
+    public ReserveInfo() {
+        promotedReserve = new double[QueryType.values().length];
+        regularReserve = new double[QueryType.values().length];
+    }
     /**
      * Returns the promoted reserve score.
      * @see <a href="http://aa.tradingagents.org/documentation">TAC Documentation</a>
      * @return the promoted reserve score.
      */
-    public final double getPromotedReserve() {
-        return promotedReserve;
+
+    public final double getPromotedReserve(final QueryType queryType) {
+        return promotedReserve[queryType.ordinal()];
     }
 
     /**
@@ -60,9 +66,9 @@ public class ReserveInfo extends AbstractTransportable {
      * @see <a href="http://aa.tradingagents.org/documentation">TAC Documentation</a>
      * @param promotedReserve the promoted reserve score.
      */
-    public final void setPromotedReserve(final double promotedReserve) {
+    public final void setPromotedReserve(final QueryType queryType, final double promotedReserve) {
         lockCheck();
-        this.promotedReserve = promotedReserve;
+        this.promotedReserve[queryType.ordinal()] = promotedReserve;
     }
 
     /**
@@ -70,8 +76,8 @@ public class ReserveInfo extends AbstractTransportable {
      * @see <a href="http://aa.tradingagents.org/documentation">TAC Documentation</a>
      * @return the regular reserve score.
      */
-    public final double getRegularReserve() {
-        return regularReserve;
+    public final double getRegularReserve(final QueryType queryType) {
+        return regularReserve[queryType.ordinal()];
     }
 
     /**
@@ -79,9 +85,9 @@ public class ReserveInfo extends AbstractTransportable {
      * @see <a href="http://aa.tradingagents.org/documentation">TAC Documentation</a>
      * @param regularReserve the regular reserve score.
      */
-    public final void setRegularReserve(final double regularReserve) {
+    public final void setRegularReserve(final QueryType queryType, final double regularReserve) {
         lockCheck();
-        this.regularReserve = regularReserve;
+        this.regularReserve[queryType.ordinal()] = regularReserve;
     }
 
     /**
@@ -91,8 +97,17 @@ public class ReserveInfo extends AbstractTransportable {
      */
     @Override
     protected final void readWithLock(final TransportReader reader) throws ParseException {
-        promotedReserve = reader.getAttributeAsDouble("promotedReserve", 0.0);
-        regularReserve = reader.getAttributeAsDouble("regularReserve", 0.0);
+//        promotedReserve = reader.getAttributeAsDouble("promotedReserve", 0.0);
+        for (QueryType type : QueryType.values()) {
+            promotedReserve[type.ordinal()] = reader.getAttributeAsDouble(
+                    String.format("promotedReserve[%s]", type.name()), 0.0);
+        }
+
+        for (QueryType type : QueryType.values()) {
+            regularReserve[type.ordinal()] = reader.getAttributeAsDouble(
+                    String.format("regularReserve[%s]", type.name()), 0.0);
+        }
+//        regularReserve = reader.getAttributeAsDouble("regularReserve", 0.0);
     }
 
     /**
@@ -101,8 +116,16 @@ public class ReserveInfo extends AbstractTransportable {
      */
     @Override
     protected final void writeWithLock(final TransportWriter writer) {
-        writer.attr("promotedReserve", promotedReserve);
-        writer.attr("regularReserve", regularReserve);
+//        writer.attr("promotedReserve", promotedReserve);
+        for (QueryType type : QueryType.values()) {
+            writer.attr(String.format("promotedReserve[%s]", type.name()),
+                    promotedReserve[type.ordinal()]);
+        }
+        for (QueryType type : QueryType.values()) {
+            writer.attr(String.format("regularReserve[%s]", type.name()),
+                    regularReserve[type.ordinal()]);
+        }
+//        writer.attr("regularReserve", regularReserve);
     }
 
     /**
@@ -123,23 +146,39 @@ public class ReserveInfo extends AbstractTransportable {
 
         ReserveInfo that = (ReserveInfo) o;
 
-        return Double.compare(that.promotedReserve, promotedReserve) == 0
-               && Double.compare(that.regularReserve, regularReserve) == 0;
+//        return Double.compare(that.promotedReserve, promotedReserve) == 0
+//               && Double.compare(that.regularReserve, regularReserve) == 0;
+
+//        if (Double.compare(that.promotedReserve, promotedReserve) != 0) {
+//            return false;
+//        }
+        if (!Arrays.equals(promotedReserve, that.promotedReserve)) {
+            return false;
+        }
+
+        if (!Arrays.equals(regularReserve, that.regularReserve)) {
+            return false;
+        }
+        return true;
 
     }
 
     /**
      * Returns the hash code from the reserve parameters.
      * @return the hash code from the reserve parameters.
+     * This needs to be corrected
      */
     @Override
     public final int hashCode() {
-        int result;
-        long temp;
-        temp = promotedReserve != +0.0d ? Double.doubleToLongBits(promotedReserve) : 0L;
-        result = (int) (temp ^ (temp >>> 32));
-        temp = regularReserve != +0.0d ? Double.doubleToLongBits(regularReserve) : 0L;
-        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        int result=0;
+//        long temp;
+//        temp = promotedReserve != +0.0d ? Double.doubleToLongBits(promotedReserve) : 0L;
+//        result = (int) (temp ^ (temp >>> 32));
+//        result = Arrays.hashCode(promotedReserve);
+//        result = 31 * result + Arrays.hashCode(regularReserve);
+
+//        temp = regularReserve != +0.0d ? Double.doubleToLongBits(regularReserve) : 0L;
+//        result = 31 * result + (int) (temp ^ (temp >>> 32));
         return result;
     }
 }
