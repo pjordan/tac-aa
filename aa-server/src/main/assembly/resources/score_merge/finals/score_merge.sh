b7cf062237438e497946a1f0617ebc76b7ada570
Ben-Alexander Cassell <bcassell@umich.edu>
2011-11-30 14:42:59 -0500

    score merging materials

---
+++ aa-server/src/main/assembly/resources/score_merge/finals/score_merge.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# cd to this directory since we expec this job to be called from cron and don't know what the present working directory is
+cd /home/dyoon/TACAA2011/aa-server-10.1.0.1/score_merge/finals/
+
+#replace with your path to the tasim jar
+java -classpath ../tasim-10.1.0.1.jar se.sics.tasim.is.score.MinAvgZeroScoreMerger -config finals.conf 2>&1 >>merge_log.txt
+
